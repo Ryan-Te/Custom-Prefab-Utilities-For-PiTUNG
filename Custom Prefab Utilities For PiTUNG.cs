a8da115f42f2e06b014e4471e5f9eba10e2a0352
Ryan T <46229874+Ryan-Te@users.noreply.github.com>
2020-09-08 09:44:44 -0400

    Update Custom Prefab Utilities For PiTUNG.cs

--- Custom Prefab Utilities For PiTUNG.cs
+++ Custom Prefab Utilities For PiTUNG.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using UnityEngine;
+using static UnityEngine.Random;
 
 class CPUFP
 {
@@ -33,10 +34,13 @@ class CPUFP
 
 			cube.transform.localScale = new Vector3(baseSize * Z, baseSize, baseSize * X);
 			cube.transform.localPosition = new Vector3(rZ * baseSize, baseSize / 2, rX * baseSize);
+			var cubeRenderer = cube.GetComponent<Renderer>();
+			cubeRenderer.material.color = new Color(1, 1, 1);
 			return obj;
 		}
 	internal static GameObject createCube(int X, int Y, int Z)
 		{
+			
 			//Create the container game object, and activate it
 			var obj = new GameObject();
 			obj.SetActive(true);
@@ -58,8 +62,66 @@ class CPUFP
 
 			cube.transform.localScale = new Vector3(baseSize * Z, baseSize * Y, baseSize * X);
 			cube.transform.localPosition = new Vector3(rZ * baseSize, baseSize * Y / 2, rX * baseSize);
+			var cubeRenderer = cube.GetComponent<Renderer>();
+			cubeRenderer.material.color = new Color(1, 1, 1);
 			return obj;
 		}
+
+	internal static GameObject createCube(int X, int Z,Color C)
+	{
+		//Create the container game object, and activate it
+		var obj = new GameObject();
+		obj.SetActive(true);
+		//Create the cube itself, bind it's position to the root object, and scale to the board grid size
+		var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		cube.transform.parent = obj.transform;
+		float rZ = 0;
+		float rX = 0;
+		if (((float)Z / 2) == Math.Round(((float)Z / 2)))
+		{
+			rZ = 0.5f;
+		}
+		if (((float)X / 2) == Math.Round(((float)X / 2)))
+		{
+			rX = 0.5f;
+		}
+
+
+
+		cube.transform.localScale = new Vector3(baseSize * Z, baseSize, baseSize * X);
+		cube.transform.localPosition = new Vector3(rZ * baseSize, baseSize / 2, rX * baseSize);
+		var cubeRenderer = cube.GetComponent<Renderer>();
+		cubeRenderer.material.color = C;
+		return obj;
+	}
+	internal static GameObject createCube(int X, int Y, int Z, Color C)
+	{
+
+		//Create the container game object, and activate it
+		var obj = new GameObject();
+		obj.SetActive(true);
+		//Create the cube itself, bind it's position to the root object, and scale to the board grid size
+		var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		cube.transform.parent = obj.transform;
+		float rZ = 0;
+		float rX = 0;
+		if (((float)Z / 2) == Math.Round(((float)Z / 2)))
+		{
+			rZ = 0.5f;
+		}
+		if (((float)X / 2) == Math.Round(((float)X / 2)))
+		{
+			rX = 0.5f;
+		}
+
+
+
+		cube.transform.localScale = new Vector3(baseSize * Z, baseSize * Y, baseSize * X);
+		cube.transform.localPosition = new Vector3(rZ * baseSize, baseSize * Y / 2, rX * baseSize);
+		var cubeRenderer = cube.GetComponent<Renderer>();
+		cubeRenderer.material.color = C;
+		return obj;
+	}
 	internal static Vector3 getPegPos(int sizeX, int sizeZ, string Side)
 		{
 			Vector3 Output;
@@ -234,4 +296,5 @@ class CPUFP
 				return Quaternion.Euler(0, 0, 0);
 			}
 		}
+
 }
